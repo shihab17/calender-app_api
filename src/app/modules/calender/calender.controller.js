516766dddb87fd3a6fff462d63b0e185b8cab24c
Shihab Ahmed Efty <shihab.ahmed@bizzntek.com>
2023-09-15 22:25:55 +0600

    update and delete api

--- src/app/modules/calender/calender.controller.js
+++ src/app/modules/calender/calender.controller.js
@@ -17,7 +17,27 @@ const create = async (req, res, next) => {
         return res.status(500).send({ error })
     }
 };
+const update = async (req, res, next) => {
+    try {
+        if (!req.params.id) return res.status(400).send({ error: { message: "id is required" } });
+        let docs = await Calender.updateOne({ _id: req.params.id }, { $set: req.body }, { new: true });
+        res.status(200).send({ result: docs });
+    } catch (error) {
+        return res.status(500).send({ error })
+    }
+};
+const remove = async (req, res, next) => {
+    try {
+        if (!req.params.id) return res.status(400).send({ error: { message: "id is required" } });
+        await Calender.deleteOne({ _id: req.params.id });
+        res.status(200).send({ result: { message: "Successfully deleted" } });
+    } catch (error) {
+        return res.status(500).send({ error })
+    }
+};
 module.exports = {
     get,
-    create
+    create,
+    remove,
+    update
 }
--- src/app/modules/calender/calender.route.js
+++ src/app/modules/calender/calender.route.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { get, create } = require('./calender.controller');
+const { get, create, update, remove } = require('./calender.controller');
 router.get('/', get);
 router.post('/', create);
+router.put('/:id', update);
+router.delete('/:id', remove);
 module.exports = router;
